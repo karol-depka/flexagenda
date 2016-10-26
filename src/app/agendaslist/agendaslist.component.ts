3107013de5bf917e135ec8e1fabb543cc9b6c00f
johnerfx <jan.sc2@vp.pl>
2016-10-26 20:05:53 +0200

    Adding newAgenda now add a first task in it.(TODO: proper task order)

--- src/app/agendaslist/agendaslist.component.ts
+++ src/app/agendaslist/agendaslist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TasksService } from '../shared/tasks.service';
+import { GeneralTasksComponent } from '../generaltasks.component';
 
 @Component({
   selector: 'agendas-list',
@@ -10,7 +11,7 @@ import { TasksService } from '../shared/tasks.service';
 export class AgendasListComponent implements OnInit {
   agendas;
   constructor(public tasksService: TasksService) { }
-
+  isGeneralVisible: boolean = false;
   ngOnInit(): void {
     this.getAgendas();
   }
@@ -22,13 +23,28 @@ export class AgendasListComponent implements OnInit {
   public addNewAgenda() {
     /*
     This function adds newAgenda object to the database.
+
     */
     var agendasList = this.tasksService.af.database.list('/agendas');
-    agendasList.push({
+    var newAgendaKey: string;
+    var newTaskKey: string;
+    newAgendaKey = agendasList.push({
       title: "NEW AGENDA TITLE",
       users: "",
       lastActive: false
-    });
+    }).key;
+    console.log(newAgendaKey);
+    var newAgenda = this.tasksService.af.database.list('/agenda_tasks/'+newAgendaKey);
+    newTaskKey = newAgenda.push({
+      order:1,
+      type:"general",
+      start:"00:00",
+      duration:"10",
+      title:"FIRST TASK",
+      description:"This is your first task in this agena. You can edit it as you please.",
+      completed:false
+    }).key;
+    console.log(newTaskKey);
   }
 
 }
