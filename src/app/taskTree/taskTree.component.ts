d2378019538bf60762f9aa15dca727cf9319ab30
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2017-03-14 04:53:25 +0100

    Added treeComponent to taskTree

--- src/app/taskTree/taskTree.component.ts
+++ src/app/taskTree/taskTree.component.ts
@@ -23,6 +23,43 @@ import { TasksListComponent } from './../taskslist/taskslist.component';
     providers: [TasksService]
 })
 export class TaskTreeComponent extends TasksListComponent {
+
+  customTemplateStringOptions = {
+    // displayField: 'subTitle',
+    isExpandedField: 'expanded',
+    idField: 'uuid',
+    // getChildren: this.getChildren.bind(this),
+    // actionMapping,
+    nodeHeight: 23,
+    allowDrag: true,
+    useVirtualScroll: true
+  }
+
+  nodes = [
+    {
+      id: 1,
+      name: 'root1',
+      children: [
+        { id: 2, name: 'child1' },
+        { id: 3, name: 'child2' }
+      ]
+    },
+    {
+      id: 4,
+      name: 'root2',
+      children: [
+        { id: 5, name: 'child2.1' },
+        {
+          id: 6,
+          name: 'child2.2',
+          children: [
+            { id: 7, name: 'subsub' }
+          ]
+        }
+      ]
+    }
+  ];
+
   constructor(
       tasksService: TasksService,
       dialog: MdDialog,
@@ -30,4 +67,5 @@ export class TaskTreeComponent extends TasksListComponent {
       /*private dragulaService: DragulaService*/) {
     super(tasksService, dialog, snackBar);
   }
+
 }
